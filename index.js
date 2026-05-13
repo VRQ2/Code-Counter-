d185cd86736706cb24c1a38348ed7989f607e49b
VRQ2 <mateusz.lukasik@student.pk.edu.pl>
2026-05-13 17:33:28 +0200

    Added line counter logic

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import fs from 'fs/promises'
+
 const directoryPath = '/home/vrq/Documents/projects';
 
-//const fs = require('fs/promises')
-import fs from 'fs/promises'
+const extOfInterest = new Set([
+    "c","cpp","bat","Makefile","sh","cc","h","hpp","pl","js","mjs","ts","py","java","rs","rb","go","cs","php","ps1","ruby","asm","as","s","nasm","jsx","tsx","hh","r","kt","ino",
+]);
 
 async function main(){
     const files = await fs.readdir(
@@ -14,17 +17,60 @@ async function main(){
 
     files.forEach(e =>{
         if(e.isFile()){
+            const lowerName = e.name.toLowerCase();
+            const splitName = e.name.split(".");
+
+            let ext = ""
+
+            let addToList = false;
+
+            if (splitName.length === 1) {
+                if(lowerName === "makefile"){
+                    addToList = true;
+                    ext = "MakeFile";
+                }
+            }else{
+                 ext = splitName[splitName.length-1];
+                if (extOfInterest.has(ext)){
+                    addToList = true;
+                }
+            }
+
+
+            if (addToList){
             filteredFiles.push(
             {
                 path: e.parentPath,
-                name: e.name
+                name: e.name,
+                ext: ext
             }
             );
+            }
         }
     });
 
-    console.log(filteredFiles);
 
+    const linesPerExt = new Map();
+
+    const p = filteredFiles.map( async e => {
+        const name = e.path + "/"+e.name;
+        const f = await fs.open(name);
+        const buf = await f.readFile();
+        f.close();
+        const txt = buf.toString("utf-8");
+        const lines = txt.split("\n");
+        const noOfLines = lines.filter(e=>
+            (e.trim() !== "")
+        ).length;
+
+        if(linesPerExt.has(e.ext)){
+            linesPerExt.set(e.ext,linesPerExt.get(e.ext)+noOfLines);
+        }else{
+            linesPerExt.set(e.ext,noOfLines)
+        }
+    });
+    await Promise.all(p);
+    console.log(linesPerExt);
 
 }
 
